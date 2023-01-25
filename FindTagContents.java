a3dedcd1d65589a591a9c2a538dd5a9c9684338c
23NateK <n.kimber22@gmail.com>
2023-01-25 12:09:20 -0600

    set up scanners for html and letter

--- FindTagContents.java
+++ FindTagContents.java
@@ -1,5 +1,15 @@
+import java.util.Scanner;
+
 public class FindTagContents {
     public static void main(String[] args) {
+        Scanner a = new Scanner(System.in);
+        System.out.println("enter html: ");
+        String html = a.nextLine();
+        Scanner b = new Scanner(System.in);
+        System.out.println("enter tag: ");
+        String letter = b.nextLine();
         
+        
+
     }
 }
