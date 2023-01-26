12cd5bc150d973d05443575a0e438de7b7d0d1c4
23NateK <n.kimber22@gmail.com>
2023-01-26 12:01:14 -0600

    Created basic FindWord code

--- FindWord.java
+++ FindWord.java
@@ -1,5 +1,22 @@
+import java.util.Scanner;
 public class FindWord {
     public static void main(String[] args) {
-        
+
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter Sentence: ");
+        String sentence = scanner.nextLine();
+        System.out.println("Enter letter: ");
+        String letter = scanner.nextLine();
+        int Lindx = sentence.indexOf(letter);
+        int startindx = sentence.lastIndexOf(" ", Lindx)+1;
+        int endindx = sentence.indexOf(" ", Lindx);
+        String word = sentence.substring(startindx,endindx);
+        System.out.println(word);
+
+
+
+
+        scanner.close();
     }
 }
