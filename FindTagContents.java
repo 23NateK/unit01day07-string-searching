197bb1c156c5e1ba19abb3e76ea7c9f62263d231
Daniel Scislowski <mr.scislowski@gmail.com>
2023-02-02 21:51:23 -0600

    FindTagContents.java feedback

--- FindTagContents.java
+++ FindTagContents.java
@@ -1,3 +1,4 @@
+// BUG: I couldn't test this code with my file input. I think it's because you made 2 Scanners. I don't think you ever need 2 scanners (you can and should use the original one to simply get more input). Please change this to only use one scanner, and make sure you can use it with files as input (LMK if you need help doing this from the terminal)
 import java.util.Scanner;
 
 public class FindTagContents {
